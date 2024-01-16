72619b51c99f5c6ce7501dffab4af8dbc59ad729
Akhilesh Mishra <83780705+akhileshmishrabiz@users.noreply.github.com>
2024-01-16 23:46:31 +0530

    Update ec2.tf

--- project-1/ec2.tf
+++ project-1/ec2.tf
@@ -16,6 +16,7 @@ resource "aws_instance" "private" {
 resource "aws_instance" "public" {
   ami                    = data.aws_ami.amazon_linux.id
   instance_type          = var.instance_type
+  subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.ssh.id, ]
   key_name               = "ssh-key"
   availability_zone      = "${data.aws_region.current.name}a"
