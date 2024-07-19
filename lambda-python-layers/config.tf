66ecaea4bdf33f1c49e7c51018b799f6d2aff4e3
Akhilesh Mishra <83780705+akhileshmishrabiz@users.noreply.github.com>
2024-07-19 12:09:12 +0000

    fix formatting

--- lambda-python-layers/config.tf
+++ lambda-python-layers/config.tf
@@ -1,19 +1,19 @@
-locals {
-  layer_definitions = [
-    {
-      "identifier" : "layers/layer1",
-      "description" : "Contains some python packages",
-      "path" : "layers/layer1",
-      "compatible_runtimes" : ["python3.8", "python3.9", "python3.10", "python3.11"]
-    },
-    {
-      "identifier" : "layers/layer2",
-      "description" : "Contains some python packages",
-      "path" : "layers/layer2",
-      "compatible_runtimes" : ["python3.8", "python3.9", "python3.10", "python3.11"]
-    },
-   
-  ]
+  locals {
+    layer_definitions = [
+      {
+        "identifier" : "layer1",
+        "description" : "Contains some python packages",
+        "path" : "layers/layer1",
+        "compatible_runtimes" : ["python3.8", "python3.9", "python3.10", "python3.11"]
+      },
+      {
+        "identifier" : "layer2",
+        "description" : "Contains some python packages",
+        "path" : "layers/layer2",
+        "compatible_runtimes" : ["python3.8", "python3.9", "python3.10", "python3.11"]
+      },
+    
+    ]
 
   # Map of layers info for the layers need to be build with this Terraform -> {layer_identifier:{layer_definitions }}
   layers_info = { for i in local.layer_definitions : i.identifier => i }
