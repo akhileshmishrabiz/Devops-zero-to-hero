9f41d8b0e4f8a378b89639d9d146c9fd39cfec2a
Akhilesh Mishra <83780705+akhileshmishrabiz@users.noreply.github.com>
2024-08-01 18:52:45 +0530

    Update main.tf

--- lambda-python-layers/main.tf
+++ lambda-python-layers/main.tf
@@ -20,23 +20,8 @@ module "layers" {
   ]
 
   runtime                  = lookup(each.value, "runtime", var.lambda_default_settings["runtime"])
-  #compatible_runtimes      = compact(distinct(concat([lookup(each.value, "runtime", var.lambda_default_settings["runtime"])], lookup(each.value, "compatible_runtimes", []))))
   store_on_s3              = true
   s3_prefix                = "layers/${each.key}"
   s3_bucket                = aws_s3_bucket.lambda_artifacts.id
   compatible_architectures = ["x86_64"]
 }
-
-# resource "aws_lambda_layer_version_permission" "lambda_layer_permission" {
-#   for_each       = local.layers_to_accounts_map
-#   layer_name     = module.layers[each.value.layer].lambda_layer_layer_arn
-#   version_number = module.layers[each.value.layer].lambda_layer_version
-#   principal      = each.value.account
-#   action         = "lambda:GetLayerVersion"
-#   statement_id   = "${each.value.layer}-${each.value.account}-${random_integer.random.result}"
-# }
-
-# resource "random_integer" "random" {
-#   min = 1
-#   max = 1000
-# }
