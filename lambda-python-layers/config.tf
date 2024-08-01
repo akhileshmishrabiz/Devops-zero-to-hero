40a87c835d8558a98c269c7ea4992410c25d031d
Akhilesh Mishra <83780705+akhileshmishrabiz@users.noreply.github.com>
2024-08-01 18:53:57 +0530

    Update config.tf

--- lambda-python-layers/config.tf
+++ lambda-python-layers/config.tf
@@ -18,16 +18,3 @@
   # Map of layers info for the layers need to be build with this Terraform -> {layer_identifier:{layer_definitions }}
   layers_info = { for i in local.layer_definitions : i.identifier => i }
 }
-
-#   # List of layers names
-#   layer_names = [for i in local.layer_definitions : i.identifier]
-
-#   # Accounts that should have permission on layer version
-#   allowed_accounts = ["AWS_ACCOUNT_ID_1", "AWS_ACCOUNT_ID_2"]
-
-#   # Mapping layers -> aws accounts for permission on layer version
-#   layers_to_accounts = flatten([for layer in local.layer_names : [for account in local.allowed_accounts : { id = "${layer}-${account}", layer = layer, account = account }]])
-  
-#   # Map to be used by for_each loop for resource
-#   layers_to_accounts_map = { for item in local.layers_to_accounts : item.id => item }
-# }
